120ca164b6e86b563c851130234269446df31fb4
Ch4rse Dev <107947345+Ch4rse404@users.noreply.github.com>
2023-04-10 16:23:59 -0300

    Create hp.py

---
+++ ini/hp.py
@@ -0,0 +1,28 @@
+print("""
+ola bem vindo ao lybe security aqui
+esta listado nossos comandos no qual
+voce pode executar dentro do terminal
+faca bom proveito
+
+fet update
+fet neofetch
+fet version
+fet devs
+fet criadores
+fet exit
+
+fet sqlmap
+fet tekshell
+fet lybedos
+fet netcat
+fet ngrok
+fet calculadora
+fet portscan
+fet nmap
+fet cep
+fet cnpj
+fet placa
+fet geoip
+fet phishing
+
+fim.""")
