4cc1107936e47543a9a8ce8bdb6d69f42f8d8e2d
Ch4rse Dev <107947345+Ch4rse404@users.noreply.github.com>
2023-04-10 16:26:03 -0300

    Create banner.py

---
+++ ini/banner.py
@@ -0,0 +1,15 @@
+print("""
+sistema criado pelo ch4rse security
+apoio lybesec d'leak
+
+baseado em linux
+
+ultilize o comando "fet" para realizar
+certas tarefas
+
+lybe security e um sistema inteligente
+que atende a necessidade dos hackers
+portando as ferramentas mais ultilizadas
+digite --help para ver nossa lista
+de comandos""")
+print("")
